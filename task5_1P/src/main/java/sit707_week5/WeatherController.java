3d68621010e33c40a993ed0d49c35ba934d605eb
easyans <ar0829@srmist.edu.in>
2026-05-07 14:58:36 +1000

    Restored task5_1P to original 5.1P submission

--- task5_1P/src/main/java/sit707_week5/WeatherController.java
+++ task5_1P/src/main/java/sit707_week5/WeatherController.java
@@ -6,105 +6,144 @@ import java.util.Date;
 import java.util.Random;
 
 public class WeatherController {
+	
+	public static final int HOURS_PER_DAY = 3;
 
-    public static final int HOURS_PER_DAY = 3;
-    private static WeatherController instance;
-    public interface Clock {
-        Date now();
-    }
-    private Clock clock = new Clock() {
-        public Date now() {
-            return new Date();
-        }
-    };
+	private static WeatherController instance;
+	
+	/**
+	 * Factory method for single instance WeatherController.
+	 * @return
+	 */
+	public static WeatherController getInstance() {
+		if (instance == null) {
+			instance = new WeatherController();
+		}
+		return instance;
+	}
+	
+	// Initialise 10 hourly temperature
+	private static double[] todaysHourlyTemperature = new double[HOURS_PER_DAY];
+	
+	/**
+	 * Private constructor prevents to create new instance manually.
+	 * A factory method needs to be used.
+	 */
+	private WeatherController() {
+		System.out.println("Creating new weather controller.");
+		
+		// sleep a while to simulate a delay
+		sleep(2 + new Random().nextInt(5));
+		
+		// Insert 10 random temperature values in today's hourly list.
+		Random random = new Random();
+		for (int i=0; i<HOURS_PER_DAY; i++) {
+			todaysHourlyTemperature[i] = 1 + random.nextInt(30);
+		}
+		System.out.println(Arrays.toString(todaysHourlyTemperature));
+	}
+	
+	public void close() {
+		System.out.println("Closing weather controller.");
+		instance = null;
+		
+		// sleep a while to simulate a delay
+		sleep(2 + new Random().nextInt(5));
+	}
+	
+	/**
+	 * Calculate minimum of today's hourly temperatures.
+	 * @return
+	 */
+	public double getTemperatureMinFromCache() {
+		double minVal = 1000;
+		for (int i=0; i<todaysHourlyTemperature.length; i++) {
+			if (minVal > todaysHourlyTemperature[i]) {
+				minVal = todaysHourlyTemperature[i];
+			}
+		}
+		return minVal;
+	}
 
-    public void setClock(Clock clock) {
-        this.clock = clock;
-    }
-
-    public static WeatherController getInstance() {
-        if (instance == null) {
-            instance = new WeatherController();
-        }
-        return instance;
-    }
-
-    private static double[] todaysHourlyTemperature = new double[HOURS_PER_DAY];
-
-    private WeatherController() {
-        System.out.println("Creating new weather controller.");
-        sleep(2 + new Random().nextInt(5));
-        Random random = new Random();
-        for (int i = 0; i < HOURS_PER_DAY; i++) {
-            todaysHourlyTemperature[i] = 1 + random.nextInt(30);
-        }
-        System.out.println(Arrays.toString(todaysHourlyTemperature));
-    }
-
-    public void close() {
-        System.out.println("Closing weather controller.");
-        instance = null;
-        sleep(2 + new Random().nextInt(5));
-    }
-
-    public double getTemperatureMinFromCache() {
-        double minVal = 1000;
-        for (int i = 0; i < todaysHourlyTemperature.length; i++) {
-            if (minVal > todaysHourlyTemperature[i]) {
-                minVal = todaysHourlyTemperature[i];
-            }
-        }
-        return minVal;
-    }
-
-    public double getTemperatureMaxFromCache() {
-        double maxVal = -1;
-        for (int i = 0; i < todaysHourlyTemperature.length; i++) {
-            if (maxVal < todaysHourlyTemperature[i]) {
-                maxVal = todaysHourlyTemperature[i];
-            }
-        }
-        return maxVal;
-    }
-
-    public double getTemperatureAverageFromCache() {
-        double sumVal = 0;
-        for (int i = 0; i < todaysHourlyTemperature.length; i++) {
-            sumVal += todaysHourlyTemperature[i];
-        }
-        return sumVal / todaysHourlyTemperature.length;
-    }
-
-    public double getTemperatureForHour(int hour) {
-        sleep(1 + new Random().nextInt(5));
-        if (hour > todaysHourlyTemperature.length) {
-            hour = 1 + new Random().nextInt(todaysHourlyTemperature.length);
-        }
-        return todaysHourlyTemperature[hour - 1];
-    }
-
-    /**
-     * Persist reported temperature to data store and return recorded time.
-     * Uses injectable clock so tests can freeze time.
-     */
-    public String persistTemperature(int hour, double temperature) {
-        SimpleDateFormat sdf = new SimpleDateFormat("H:m:s");
-        // Uses injected clock (real or fake)
-        String savedTime = sdf.format(clock.now());
-        System.out.println("Temperature: " + temperature + " of hour: " + hour + ", saved at " + savedTime);
-        sleep(1 + new Random().nextInt(2));
-        return savedTime;
-    }
-
-    public int getTotalHours() {
-        return todaysHourlyTemperature.length;
-    }
-
-    public static void sleep(int sec) {
-        try {
-            Thread.sleep(sec * 1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+	/**
+	 * Calculate maximum of today's hourly temperatures.
+	 * @return
+	 */
+	public double getTemperatureMaxFromCache() {
+		double maxVal = -1;
+		for (int i=0; i<todaysHourlyTemperature.length; i++) {
+			if (maxVal < todaysHourlyTemperature[i]) {
+				maxVal = todaysHourlyTemperature[i];
+			}
+		}
+		return maxVal;
+	}
+	
+	/**
+	 * Calculate average of today's hourly temperatures.
+	 * @return
+	 */
+	public double getTemperatureAverageFromCache() {
+		double sumVal = 0;
+		for (int i=0; i<todaysHourlyTemperature.length; i++) {
+			sumVal += todaysHourlyTemperature[i];
+		}
+		return sumVal/todaysHourlyTemperature.length;
+	}
+	
+	/**
+	 * Return temperature for given hour of current day.
+	 * @param hour
+	 * @return
+	 */
+	public double getTemperatureForHour(int hour) {
+		// sleep a while to simulate a delay
+		sleep(1 + new Random().nextInt(5));
+		
+		// Let's return a randomly selected temperature from hourly list if hour does not exist.
+		if (hour > todaysHourlyTemperature.length) {
+			hour = 1 + new Random().nextInt(todaysHourlyTemperature.length);
+		}
+		
+		// Hour index starts from 0 instead of 1 due to array indexing.
+		return todaysHourlyTemperature[hour-1];
+	}
+	
+	/**
+	 * Persist reported temperature to data store and return recorded time. 
+	 * @param hour
+	 * @param temperature
+	 * @return
+	 */
+	public String persistTemperature(int hour, double temperature) {
+		SimpleDateFormat sdf = new SimpleDateFormat("H:m:s");
+		String savedTime = sdf.format(new Date());
+		System.out.println("Temperature: " + temperature + " of hour: " + hour + ", saved at " + savedTime);
+		
+		// sleep a while to simulate a delay
+		sleep(1 + new Random().nextInt(2));
+		
+		return savedTime;
+	}
+	
+	/**
+	 * Calculated the number of hours temperature data is available for today.
+	 * @return
+	 */
+	public int getTotalHours() {
+		return todaysHourlyTemperature.length;
+	}
+	
+	/**
+	 * Sleep for specified seconds.
+	 * @param sec
+	 */
+	public static void sleep(int sec) {
+		try {
+			Thread.sleep(sec*1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- task5_1P/src/test/java/sit707_week5/WeatherControllerTest.java
+++ task5_1P/src/test/java/sit707_week5/WeatherControllerTest.java
@@ -1,8 +1,4 @@
 package sit707_week5;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -10,108 +6,90 @@ import org.junit.Test;
 
 public class WeatherControllerTest {
 
-    private static WeatherController wController;
-    private static int nHours;
-    private static double[] hourlyTemperatures;
+	private static WeatherController wController;
+	private static int nHours;
+	private static double[] hourlyTemperatures;
 
-    @BeforeClass
-    public static void setUpBeforeClass() {
-        System.out.println("+++ setUpBeforeClass +++");
-        wController = WeatherController.getInstance();
-        nHours = wController.getTotalHours();
-        hourlyTemperatures = new double[nHours];
-        for (int i = 0; i < nHours; i++) {
-            hourlyTemperatures[i] = wController.getTemperatureForHour(i + 1);
-        }
-    }
+	// ARRANGE 
+	@BeforeClass
+	public static void setUpBeforeClass() {
+		System.out.println("+++ setUpBeforeClass +++");
+		wController = WeatherController.getInstance();
+		nHours = wController.getTotalHours();
+		hourlyTemperatures = new double[nHours];
+		for (int i = 0; i < nHours; i++) {
+			hourlyTemperatures[i] = wController.getTemperatureForHour(i + 1);
+		}
+	}
 
-    @AfterClass
-    public static void tearDownAfterClass() {
-        System.out.println("+++ tearDownAfterClass +++");
-        wController.close();
-    }
+	// AFTER - runs ONCE after all tests (handles will slow shutdown)
+	@AfterClass
+	public static void tearDownAfterClass() {
+		System.out.println("+++ tearDownAfterClass +++");
+		wController.close();
+	}
 
-    @Test
-    public void testStudentIdentity() {
-        String studentId = "s224393151";
-        Assert.assertNotNull("Student ID is null", studentId);
-    }
+	@Test
+	public void testStudentIdentity() {
+		String studentId = "s224393151";
+		Assert.assertNotNull("Student ID is null", studentId);
+	}
 
-    @Test
-    public void testStudentName() {
-        String studentName = "Aakash Kumar Raj Sakhineti";
-        Assert.assertNotNull("Student name is null", studentName);
-    }
+	@Test
+	public void testStudentName() {
+		String studentName = "Aakash Kumar Raj Sakhineti";
+		Assert.assertNotNull("Student name is null", studentName);
+	}
 
-    @Test
-    public void testTemperatureMin() {
-        System.out.println("+++ testTemperatureMin +++");
+	@Test
+	public void testTemperatureMin() {
+		System.out.println("+++ testTemperatureMin +++");
 
-        double minTemperature = 1000;
-        for (int i = 0; i < nHours; i++) {
-            if (minTemperature > hourlyTemperatures[i]) {
-                minTemperature = hourlyTemperatures[i];
-            }
-        }
+		// ACT - use shared hourlyTemperatures array (fast, no slow calls)
+		double minTemperature = 1000;
+		for (int i = 0; i < nHours; i++) {
+			if (minTemperature > hourlyTemperatures[i]) {
+				minTemperature = hourlyTemperatures[i];
+			}
+		}
 
-        Assert.assertTrue(wController.getTemperatureMinFromCache() == minTemperature);
-    }
+		// ASSERT
+		Assert.assertTrue(wController.getTemperatureMinFromCache() == minTemperature);
+	}
 
-    @Test
-    public void testTemperatureMax() {
-        System.out.println("+++ testTemperatureMax +++");
+	@Test
+	public void testTemperatureMax() {
+		System.out.println("+++ testTemperatureMax +++");
 
-        double maxTemperature = -1;
-        for (int i = 0; i < nHours; i++) {
-            if (maxTemperature < hourlyTemperatures[i]) {
-                maxTemperature = hourlyTemperatures[i];
-            }
-        }
+		// ACT - use shared hourlyTemperatures array (fast, no slow calls)
+		double maxTemperature = -1;
+		for (int i = 0; i < nHours; i++) {
+			if (maxTemperature < hourlyTemperatures[i]) {
+				maxTemperature = hourlyTemperatures[i];
+			}
+		}
 
-        Assert.assertTrue(wController.getTemperatureMaxFromCache() == maxTemperature);
-    }
+		// ASSERT
+		Assert.assertTrue(wController.getTemperatureMaxFromCache() == maxTemperature);
+	}
 
-    @Test
-    public void testTemperatureAverage() {
-        System.out.println("+++ testTemperatureAverage +++");
+	@Test
+	public void testTemperatureAverage() {
+		System.out.println("+++ testTemperatureAverage +++");
 
-        double sumTemp = 0;
-        for (int i = 0; i < nHours; i++) {
-            sumTemp += hourlyTemperatures[i];
-        }
-        double averageTemp = sumTemp / nHours;
+		// ACT - use shared hourlyTemperatures array (fast, no slow calls)
+		double sumTemp = 0;
+		for (int i = 0; i < nHours; i++) {
+			sumTemp += hourlyTemperatures[i];
+		}
+		double averageTemp = sumTemp / nHours;
 
-        Assert.assertTrue(wController.getTemperatureAverageFromCache() == averageTemp);
-    }
+		// ASSERT
+		Assert.assertTrue(wController.getTemperatureAverageFromCache() == averageTemp);
+	}
 
-    @Test
-    public void testTemperaturePersist() {
-        System.out.println("+++ testTemperaturePersist +++");
-        final Date frozenTime = new Date();
-        SimpleDateFormat sdf = new SimpleDateFormat("H:m:s");
-        final String expectedTime = sdf.format(frozenTime);
-        wController.setClock(new WeatherController.Clock() {
-            public Date now() {
-                return frozenTime;
-            }
-        });
+	@Test
+	public void testTemperaturePersist() {
 
-        int testHour = 1;
-        double testTemperature = hourlyTemperatures[testHour - 1];
-        String persistedTime = wController.persistTemperature(testHour, testTemperature);
-
-        System.out.println("Expected (frozen) time : " + expectedTime);
-        System.out.println("Persisted timestamp    : " + persistedTime);
-        Assert.assertEquals(
-            "Persisted time does not match frozen clock time",
-            expectedTime,
-            persistedTime
-        );
-
-        wController.setClock(new WeatherController.Clock() {
-            public Date now() {
-                return new Date();
-            }
-        });
-    }
+	}
 }
